5e86832916fc5d5d23d0f43a88066bc6e5425c89
Graham Ollis <perl@wdlabs.com>
2014-10-09 06:52:46 -0400

    version 0.02

--- lib/Acme/Ford/Prefect/FFI.pm
+++ lib/Acme/Ford/Prefect/FFI.pm
@@ -37,7 +37,7 @@ L<Acme::Alien::DontPanic> which provides the needed libdontpanic library to be a
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 $VERSION = eval $VERSION;
 
 our($dll) = Acme::Alien::DontPanic->dynamic_libs;
